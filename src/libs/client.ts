0b2bcfa5e64b98487a129f446975fdcf98be3bc5
Yuki Umeda <100minus1birds.equal.swan@gmail.com>
2025-03-17 22:43:38 +0900

    microCMS 連携 test

---
+++ src/libs/client.ts
@@ -0,0 +1,36 @@
+import { createClient } from 'microcms-js-sdk';
+
+// TODO: 要整備
+export type Cast = {
+  name: string;
+};
+
+if (!process.env.SERVICE_DOMAIN) {
+  throw new Error('MICROCMS_SERVICE_DOMAIN is required');
+}
+
+if (!process.env.API_KEY) {
+  throw new Error('MICROCMS_API_KEY is required');
+}
+
+export const client = createClient({
+  serviceDomain: process.env.SERVICE_DOMAIN,
+  apiKey: process.env.API_KEY,
+});
+
+// 出演者一覧を取得
+export const getCasts = async () => {
+  const casts = await client.getList<Cast>({
+    endpoint: 'casts',
+  });
+  return casts;
+};
+
+// 出演者の詳細を取得
+export const getCastDetail = async (contentId: string) => {
+  const cast = await client.getListDetail<Cast>({
+    endpoint: 'casts',
+    contentId,
+  });
+  return cast;
+};
