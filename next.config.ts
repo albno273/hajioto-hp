2e40f03d9dd7d5a9ca32e5a993c75dd4824b1200
Yuki Umeda <100minus1birds.equal.swan@gmail.com>
2025-03-21 22:38:47 +0900

    ⚡️ 手元で SSG できるところまで完了

--- next.config.ts
+++ next.config.ts
@@ -1,25 +1,21 @@
-import path from 'path';
-import type { NextConfig } from 'next';
+import path from "path";
+import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   reactStrictMode: true,
-
+  output: "export",
+  trailingSlash: true,
   eslint: {
-    dirs: ['src'],
+    dirs: ["src"],
   },
-
   compiler: {
     styledComponents: true,
   },
-
-  pageExtensions: ['page.tsx', 'page.ts'],
-
   images: {
-    domains: ['images.microcms-assets.io'],
+    domains: ["images.microcms-assets.io"],
   },
-
   sassOptions: {
-    includePaths: [path.join(__dirname, 'src/styles')],
+    includePaths: [path.join(__dirname, "src/styles")],
   },
 };
 
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,42 +1,31 @@
-import Image from 'next/image';
-import styles from './page.module.css';
+// import Image from "next/image";
+import styles from "@/styles/views/Home.module.scss";
 
-import { getCasts } from '@/libs/client';
+import { getCasts } from "@/libs/client";
+import { NextPage } from "next";
 
-export default async function Home() {
+const fetchCasts = async () => {
   const casts = await getCasts();
+
+  return {
+    title: casts.contents[0].name,
+  };
+};
+
+type PageProps = {
+  title: string;
+};
+
+const Home: NextPage<PageProps> = async () => {
+  const casts = await fetchCasts();
 
   return (
     <div className={styles.page}>
       <main className={styles.main}>
-        <p>{casts.contents[0].name}</p>
+        <p>{casts.title}</p>
       </main>
-      <footer className={styles.footer}>
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image aria-hidden src="/file.svg" alt="File icon" width={16} height={16} />
-          Learn
-        </a>
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image aria-hidden src="/window.svg" alt="Window icon" width={16} height={16} />
-          Examples
-        </a>
-        <a
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image aria-hidden src="/globe.svg" alt="Globe icon" width={16} height={16} />
-          Go to nextjs.org →
-        </a>
-      </footer>
     </div>
   );
-}
+};
+
+export default Home;
