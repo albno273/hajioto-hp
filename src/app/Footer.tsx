a9ff7e065ba05816da9664d7729f1d895cfd8174
outcry801 <eiki.trp@gmail.com>
2025-04-02 23:52:35 +0900

    Footer.tsx
    Footerをコンポーネント化しました。

---
+++ src/app/Footer.tsx
@@ -0,0 +1,11 @@
+import Footerstyles from "@/styles/views/Footer.module.scss";
+
+const Footer = () => {
+    return (
+      <footer className={Footerstyles.Footer}>
+        <p>this is footer</p>
+      </footer>
+    );
+  };
+  
+  export default Footer;
