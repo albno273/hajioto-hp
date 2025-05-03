5338254c4af90b8d426a305a1b38e015a5d5ea66
Yuki Umeda <100minus1birds.equal.swan@gmail.com>
2025-05-04 03:43:47 +0900

    ⚡️ Google map のサイズ修正

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -24,7 +24,6 @@ const Home: React.FC = async () => {
         <div className={s.map}>
           <iframe
             src='https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3241.7308750073453!2d139.69272851193134!3d35.65900173107938!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x60188caa3c015555%3A0xa54d4d3a970d7889!2sclub%20asia!5e0!3m2!1sen!2sjp!4v1744817104019!5m2!1sen!2sjp'
-            width='400'
             height='300'
             loading='lazy'
           />
